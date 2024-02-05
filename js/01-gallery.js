847b34a257f51af0a9aadc6b4406179c82214c72
achoma <139071702+achoma@users.noreply.github.com>
2024-02-05 22:02:00 +0100

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector(".gallery")
+for (const item of galleryItems){
+    const images = `
+        <div class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+            <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+            />
+            </a>
+        </div> `;
+    gallery.insertAdjacentHTML(`beforeend`, images)
+}
+gallery.addEventListener("click",(event) => {
+    event.preventDefault();
+    // console.log(event.target);
+    const lightbox = basicLightbox.create(`<img src="${event.target.dataset.source}" alt="${event.target.alt}" />>`);
+    lightbox.show();
+    } )
+
+
+
+
 
 console.log(galleryItems);
