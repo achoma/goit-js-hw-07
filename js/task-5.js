cfca68ce051dfbff01fb382e97835be173f408d3
achoma <139071702+achoma@users.noreply.github.com>
2024-09-17 19:09:43 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,13 @@
+const background = document.querySelector(".widget");
+const btn = document.querySelector("button");
+const colorSpan = document.querySelector("span.color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+btn.addEventListener("click", () => {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorSpan.textContent = getRandomHexColor();
+});
