dc215ac7ba313f99eebe39a4c9881d5482ce536d
achoma <139071702+achoma@users.noreply.github.com>
2024-09-19 00:52:29 +0200

    task-6

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,34 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+document.querySelector("[data-create]").addEventListener("click", createBoxes);
+document
+  .querySelector("[data-destroy]")
+  .addEventListener("click", destroyBoxes);
+
+const inputField = document.querySelector("input");
+const boxesField = document.querySelector("#boxes");
+
+function createBoxes() {
+  const inputValue = inputField.value;
+  if (inputValue < 1 || inputValue > 100) {
+    inputField.value = "";
+    return;
+  }
+  const box = document.createDocumentFragment();
+  for (let i = 0; i < inputValue; i++) {
+    const boxes = document.createElement("div");
+    boxes.style.width = `${30 + i * 10}px`;
+    boxes.style.height = `${30 + i * 10}px`;
+    boxes.style.backgroundColor = getRandomHexColor();
+    box.appendChild(boxes);
+  }
+  boxesField.prepend(box);
+  inputField.value = "";
+}
+
+function destroyBoxes() {
+  boxesField.innerHTML = "";
+  inputField.value = "";
+}
