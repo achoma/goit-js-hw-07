1e592083ea5afc02ddf8c21f653058a8fed73a35
achoma <139071702+achoma@users.noreply.github.com>
2024-09-19 19:45:08 +0200

    task-3 update

--- js/task-3.js
+++ js/task-3.js
@@ -4,5 +4,7 @@ const output = document.querySelector("#name-output");
 textInput.addEventListener("input", (event) => {
   if (event.currentTarget.value.trim() !== "") {
     output.textContent = event.currentTarget.value;
+  } else {
+    output.textContent = "Anonymous";
   }
 });
